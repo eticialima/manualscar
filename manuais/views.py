8273f669daf374e86e0d511b3f59718d068a9228
eticialima <contato.leticialima.me@gmail.com>
2021-06-13 14:16:25 -0300

    teste
    teste

---
+++ manuais/views.py
@@ -0,0 +1,107 @@
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic.base import TemplateView 
+from manuais.forms import EquipamentosForm, RevisaomanualForm
+from manuais.models import Equipamentos, RevisaoManuais
+
+# --- equipamentos views --- #
+class EquipamentosIndexView(TemplateView):
+	template_name = 'equipamento/index-equipamentos.html'
+
+class EquipamentosNewView(CreateView):
+	template_name = 'equipamento/equipamento-novo.html'
+	form_class = EquipamentosForm
+	success_url = reverse_lazy('equipamentos')
+	success_message = 'Equipamento Cadastrado com sucesso'
+
+class EquipamentosDeleteView(DeleteView):
+	model = Equipamentos
+	template_name = 'equipamento/equipamento-delete.html'
+	success_url = reverse_lazy('equipamentos')
+	success_message = 'O equipamento foi deletado com sucesso'
+
+class EquipamentosUpdateView(UpdateView):
+	model = Equipamentos
+	form_class = EquipamentosForm
+	template_name = 'equipamento/equipamento-alterar.html'
+	success_url = reverse_lazy('equipamentos')
+	success_message = 'As alterações foram efectuadas com sucesso'
+
+class EquipamentosListView(ListView):
+	model = Equipamentos
+	template_name = 'equipamento/equipamentos.html'
+
+# --- manuais views --- #
+class ManuaisIndexView(TemplateView):
+	template_name = 'manuais/index-manuais.html'
+
+class ManuaisNewView(CreateView):
+	template_name = 'manuais/manual-novo.html'
+	form_class = RevisaomanualForm
+	model = RevisaoManuais 
+	
+	def get(self, request, *args, **kwargs):    
+		equipamento = self.nome_equipamento
+		form = RevisaomanualForm()
+
+		if request.nome_equipamento.is_valid():
+			manuais = RevisaoManuais.objects.filter(nome_equipamento=equipamento)
+			form.field['nome_equipamento'].queryset = manuais 
+		context = {'form': form} 
+		return render(request, 'manuais/manual-novo.html', context)
+
+	def post(self, request, *args, **kwargs):
+		form = RevisaomanualForm(request.POST, request.FILES)
+		equipamento = self.request.nome_equipamento
+		nome_equipamento = request.POST.get('nome_equipamento') 
+
+		if 'btn_adicionar' in self.request.POST: 
+			if Equipamentos.objects.filter(nome_equipamento=equipamento).filter(
+					nome_equipamento=nome_equipamento).exists(): 
+				context = {'form': form} 
+				messages.error(request, 'err') 
+				return render(request, 'manuais/manual-novo.html', context)
+
+			if form.is_valid():
+				form_model = form.save(commit=False)
+				form_model.nome_equipamento = self.request.nome_equipamento
+				form_model.save()
+				form.save()
+
+				messages.success(request, 'save')
+				return redirect('equipamentos')
+
+			else:
+				messages.error(request, 'Erro ao cadastrar') 
+				context = {'form': form}
+				return render(request, 'manuais/manual-novo.html', context)
+
+	
+	def get_success_url(self) -> str:
+		messages.success(self.request, 'O manual foi Cadastrado com sucesso')
+		return reverse_lazy('equipamentos')
+
+class ManuaisDeleteView(DeleteView):
+	model = RevisaoManuais  
+	template_name = 'manuais/manual-delete.html' 
+
+	def get_success_url(self):
+		messages.success(self.request, 'O manual foi deletado com sucesso')
+		return reverse_lazy('manuais', kwargs = {'pk': self.object.nome_equipamento.id })
+
+class ManuaisUpdateView(UpdateView):
+    model = RevisaoManuais
+    form_class = RevisaomanualForm
+    template_name = 'manuais/manual-alterar.html'
+    def get_success_url(self) -> str:
+        messages.success(self.request, 'Manual Cadastrado com sucesso')
+        return reverse_lazy('equipamentos')
+
+class ManuaisListView(ListView):
+	model = RevisaoManuais
+	template_name = 'manuais/manuais.html'
+	context_object_name = 'manuais_list' 
+	def get_queryset(self):  
+		return RevisaoManuais.objects.filter(nome_equipamento_id=self.kwargs['pk'])
