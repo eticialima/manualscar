352424b9d5b26d7e787ff03acc1c5ef478167d75
eticialima <contato.leticialima.me@gmail.com>
2021-06-26 21:18:24 -0300

    Projeto_base_Django_ClassBasedView
    Projeto_base_Django_ClassBasedView cadastro de equipamentos

--- app_equipamentos/settings.py
+++ app_equipamentos/settings.py
@@ -27,7 +27,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # ----------------------------------------------------------
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a-^cwq-nzm(b-oi%25(2mzwu6k=m8vl*fnkjz(!lv!$5b$_qnh'
+SECRET_KEY = 'KEY HERE'
 
 # ----------------------------------------------------------
 # SECURITY WARNING: don't run with debug turned on in production!
